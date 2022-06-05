be2d9fae08cb43e3560cb3abe392ed71cf749b5f
plibi <plbr0908@gmail.com>
2022-06-06 00:48:41 +0900

    streamlit deploy test

--- app.py
+++ app.py
@@ -8,19 +8,15 @@ import numpy as np
 # 글씨 색상
 # https://discuss.streamlit.io/t/a-way-to-build-your-own-unique-text-and-header/13943
 
-# Set wide web page
+
+# ====================== Web Page Setting =======================
+# Set wide mode
 st.set_page_config(layout="wide")
 
-# Title
-st.subheader("영화 장르별 핵심 키워드")
-
+# Font test
+st.write('글씨 폰트 테스트')
 original_title = '<p style="font-family:Courier; color:Blue; font-size: 17px;">Original Image</p>'
 st.markdown(f'``{original_title}``', unsafe_allow_html=True)
-
-genre_list = [
-    '다큐멘터리', '뮤지컬', '전쟁', '액션', '공포',
-    '느와르', '모험', 'SF', '스릴러', '범죄', '멜로애정로맨스',
-    '판타지', '코미디', '애니메이션', '미스터리', '가족', '드라마']
 
 
 # Button Custormizing
@@ -30,6 +26,7 @@ div.stButton > button:first-child {
     background-color: rgb(255, 255, 255);
 }
 </style>""", unsafe_allow_html=True)
+
 
 # cols = st.columns(17)
 # with cols[0]:
@@ -44,29 +41,6 @@ div.stButton > button:first-child {
 #     musical = st.button('musical')
 #     war = st.button('war')
 #     action = st.button('action')
-
-
-# _, _, _, col, _, _, _ = st.columns([1]*6+[1.18])
-# clicked = col.button('Button')
-
-# col1, col2, col3 , col4, col5, _, _, _, _, _, _, _, _ = st.columns(13)
-# col1
-# col1.button('drama')
-# col2.button('documentary')
-# col3.button('musical')
-# col4.button('war')
-# col5.button('action')
-# with col1:
-#     drama = st.button('drama',key="hi", kwargs={'class':'hi'})
-# with col2:
-#     documentary = st.button('documentary')
-# with col4:
-#     musical = st.button('musical')
-# with col5:
-#     war = st.button('war')
-# with col3 :
-#     action = st.button('action')
-
 
 
 # if True:
@@ -116,14 +90,11 @@ div.stButton > button:first-child {
 #     """,
 #         unsafe_allow_html=True,
 #     )
-
-# "# Hello"
-
 # """This font will look different, based on your choice of radio button"""
 
 
-
-PATH = 'C:/Users/박재현/Desktop/github/Service/data/'
+# ======================= Define Function =======================
+PATH = 'data/'
 # Data Loading Function
 def getdata(genre):
     file_name = f'{genre}_keyword_result.pkl'
@@ -132,7 +103,28 @@ def getdata(genre):
     return data
 
 
-# Visualizing Function
+# Print top 10 keyword
+def getkeyword(dataframe):
+        keyword_list = dataframe['word'].tolist()
+        top_10 = ''
+        for word in keyword_list[:10]:
+            top_10 += f' **``{word}``**'
+
+        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
+        st.markdown(f'##### {top_10}')
+        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
+
+
+# Print similar word of keyword
+def getsimilar(dataframe):
+    st.markdown('##### 연관 키워드')
+    for i in range(10):
+        keyword = dataframe['word'][i]
+        similar = dataframe['similar_word'][i]
+        st.markdown(f'**``{keyword}``** 유사한 키워드: **{similar}**')
+
+
+# Keyword ratio visualizing function
 def visualize(DataFrame, top):
     top_3per = (DataFrame['all_ratio'] >= 3).sum()
     if top:
@@ -162,16 +154,10 @@ def visualize(DataFrame, top):
     options = {
             'title': {
             'title': title,
-            # 'subtitle': 'Fake Data',
-            # 'pos_left': 'center'
         },
     }
     b = (
         Pie(options).add("", values, radius=[40, 120]).set_global_opts(
-            # init_opts=opts.InitOpts(
-            #     width='1600px',
-            #     height='800px',
-            # ),
             title_opts=opts.TitleOpts(
                 **options.get('title'),
                 pos_left=True,
@@ -184,222 +170,56 @@ def visualize(DataFrame, top):
             tooltip_opts=opts.TooltipOpts(
                 position='inside',
             ),
-            
-            # visualmap_opts=opts.VisualMapOpts(
-            #     show
-            # )
-
         ).set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}%"))
     )
     st_pyecharts(b)
 
 
-# # 1. Button
-# c1, c2, c3, c4, c5 = st.columns(5)
-# c1 = st.button('드라마')
-# c2 = st.button('다큐멘터리')
-# c3 = st.button('뮤지컬')
-# c4 = st.button('전쟁')
-# c5 = st.button('액션')
-def getkeywordlist(dataframe):
-    keyword_list = dataframe['word'].tolist()
-    result = ' '.join(keyword_list)
-    return result
-
-if st.button("드라마"):
-    column1, column2 = st.columns(2)
-    column3, column4 = st.columns(2)
-
-    genre = '드라마'
+# Result Function
+def getresult(genre):
     df = getdata(genre)
-    keyword_list = df['word'].tolist()
-    
-    # Show Part
-    ## Keyword
-    with column1:
-        st.write('\n')
-        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
-        # t_result = '**`` '.join(keyword_list[:10])
-        # st.markdown(t_result)
-        result = f'**``{keyword_list[0]}``**'
-        for keyword in keyword_list[1:10]:
-            result += f' **``{keyword}``**'
-        st.markdown(f'##### {result}')
-        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
-    
-    ## Similar word
-    with column3:
-        st.markdown('##### 연관 키워드')
-        for i in range(10):
-            keyword = df['word'][i]
-            similar = df['similar_word'][i]
-            st.markdown(f'**``{keyword}``** 유사한 키워드: **{similar}**')
-
-    # Keyword Ratio Visualization
-    with column2:
-        st.write('\n')
-        visualize(df, False)
-
-    with column4:
-        st.write('\n')
-        st.write('\n')
-        visualize(df, True)
-    
-    
-    
-if st.button('액션'): ##
-    column1, column2 = st.columns(2)
-    column3, column4 = st.columns(2)
-
-    genre = '액션'
-    df = getdata(genre)
-    keyword_list = df['word'].tolist()
-    
-    # Show Part
-    ## Keyword
-    with column1:
-        st.write('\n')
-        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
-        # t_result = '**`` '.join(keyword_list[:10])
-        # st.markdown(t_result)
-        result = f'**``{keyword_list[0]}``**'
-        for keyword in keyword_list[1:10]:
-            result += f' **``{keyword}``**'
-        st.markdown(f'##### {result}')
-        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
-    
-    ## Similar word
-    with column3:
-        st.markdown('##### 연관 키워드')
-        for i in range(10):
-            keyword = df['word'][i]
-            similar = df['similar_word'][i]
-            st.markdown(f'**``{keyword}``** 유사한 키워드: **{similar}**')
-
-    # Keyword Ratio Visualization
-    with column2:
-        st.write('\n')
-        visualize(df, False)
-
-    with column4:
-        st.write('\n')
-        st.write('\n')
-        visualize(df, True)
-
-if st.button('판타지'): ##
-    column1, column2 = st.columns(2)
-    column3, column4 = st.columns(2)
-
-    genre = '판타지'
-    df = getdata(genre)
-    keyword_list = df['word'].tolist()
-    
-    # Show Part
-    ## Keyword
-    with column1:
-        st.write('\n')
-        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
-        # t_result = '**`` '.join(keyword_list[:10])
-        # st.markdown(t_result)
-        result = f'**``{keyword_list[0]}``**'
-        for keyword in keyword_list[1:10]:
-            result += f' **``{keyword}``**'
-        st.markdown(f'##### {result}')
-        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
-    
-    ## Similar word
-    with column3:
-        st.markdown('##### 연관 키워드')
-        for i in range(10):
-            keyword = df['word'][i]
-            similar = df['similar_word'][i]
-            st.markdown(f'**``{keyword}``** 유사한 키워드: **{similar}**')
-
-    # Keyword Ratio Visualization
-    with column2:
-        st.write('\n')
-        visualize(df, False)
-
-    with column4:
-        st.write('\n')
-        st.write('\n')
-        visualize(df, True)
+    ...
 
 
+# ======================== Main Contents ========================
+# Title
+st.subheader("영화 장르별 핵심 키워드")
 
-if st.button('애니메이션'): ##
-    column1, column2 = st.columns(2)
-    column3, column4 = st.columns(2)
+genre_list = [
+    '드라마', '액션', '판타지', 'SF', '스릴러',
+    '코미디', '가족', '다큐멘터리', '뮤지컬', '전쟁',
+    '공포', '느와르', '모험', '범죄', '멜로애정로맨스',
+    '애니메이션', '미스터리']
 
-    genre = '애니메이션'
-    df = getdata(genre)
-    keyword_list = df['word'].tolist()
-    
-    # Show Part
-    ## Keyword
-    with column1:
-        st.write('\n')
-        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
-        # t_result = '**`` '.join(keyword_list[:10])
-        # st.markdown(t_result)
-        result = f'**``{keyword_list[0]}``**'
-        for keyword in keyword_list[1:10]:
-            result += f' **``{keyword}``**'
-        st.markdown(f'##### {result}')
-        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
-    
-    ## Similar word
-    with column3:
-        st.markdown('##### 연관 키워드')
-        for i in range(10):
-            keyword = df['word'][i]
-            similar = df['similar_word'][i]
-            st.markdown(f'**``{keyword}``** 유사한 키워드: **{similar}**')
+# Contents by genre
+for genre in genre_list:
+    if st.button(genre):
+        column1, column2 = st.columns(2)
+        column3, column4 = st.columns(2)
 
-    # Keyword Ratio Visualization
-    with column2:
-        st.write('\n')
-        visualize(df, False)
+        df = getdata(genre)
 
-    with column4:
-        st.write('\n')
-        st.write('\n')
-        visualize(df, True)
+        # 1. Keyword
+        with column1:
+            st.write('\n')
+            getkeyword(df)
 
+        # 2. Similar word
+        with column3:
+            getsimilar(df)
 
-if st.button('다큐멘터리'):
-    st.write('준비 중...')
-if st.button('뮤지컬'):
-    st.write('준비 중...')
-if st.button('전쟁'):
-    st.write('준비 중...')
-if st.button('공포'): ##
-    st.write('준비 중...')
-if st.button('느와르'):
-    st.write('준비 중...')
-if st.button('모험'):
-    st.write('준비 중...')
-if st.button('SF'):
-    st.write('준비 중...')
-if st.button('스릴러'): ##
-    st.write('준비 중...')
-if st.button('범죄'):
-    st.write('준비 중...')
-if st.button('멜로애정로맨스'):
-    st.write('준비 중...')
-if st.button('코미디'): ##
-    st.write('준비 중...')
-if st.button('미스터리'):
-    st.write('준비 중...')
-if st.button('가족'):
-    st.write('준비 중...')
-
+        # 3. Keyword Ratio
+        with column2:       # Total
+            st.write('\n')
+            visualize(df, False)
+        with column4:       # Only top 10
+            st.write('\n')
+            visualize(df, True)
 
 
 # Sidebar
-st.sidebar.header('설명')
+st.sidebar.header('사용 방법')
 text = '**``장르``** 를 선택하면 해당 장르의 **``핵심 키워드``** 와 **``연관 키워드``**, **``키워드별 중요도``** 를 보여줍니다.'
 st.sidebar.markdown(text)
 st.sidebar.write('영화 외에도 더 많은 분야/산업 추가 예정입니다.')
-
 
