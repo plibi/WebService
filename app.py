0378bc596ce5c0cbedf9511eec8fd022c7c31496
plibi <plbr0908@gmail.com>
2022-06-07 13:56:46 +0900

    fix page

--- app.py
+++ app.py
@@ -105,14 +105,14 @@ def getdata(genre):
 
 # Print top 10 keyword
 def getkeyword(dataframe):
-        keyword_list = dataframe['word'].tolist()
-        top_10 = ''
-        for word in keyword_list[:10]:
-            top_10 += f' **``{word}``**'
+    keyword_list = dataframe['word'].tolist()
+    top_10 = ''
+    for word in keyword_list[:10]:
+        top_10 += f' **``{word}``**'
 
-        st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
-        st.markdown(f'##### {top_10}')
-        st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
+    st.markdown(f'##### _{genre}_ 장르의 핵심 키워드')
+    st.markdown(f'##### {top_10}')
+    st.markdown(f'등 총 {len(keyword_list)}개의 키워드 중 **상위 10개**의 키워드')
 
 
 # Print similar word of keyword
@@ -160,15 +160,15 @@ def visualize(DataFrame, top):
         },
     }
     b = (
-        Pie(options).add("", values, radius=[40, 120]).set_global_opts(
+        Pie(options).add("", values, radius=[40, 100]).set_global_opts(
             title_opts=opts.TitleOpts(
                 **options.get('title'),
                 pos_left=True,
             ),
             legend_opts=opts.LegendOpts(
-                pos_right='8%',
-                pos_top="20%",
-                orient=True,
+                # pos_right='8%',
+                pos_top="93%",
+                # orient=True,
             ),
             tooltip_opts=opts.TooltipOpts(
                 position='inside',
@@ -211,32 +211,32 @@ st.write('=> ``실화``를 바탕으로 한 소재가 깊은 인상을 남김')
 # st.write('=> 스토리 유사 키워드 확인')
 
 # Contents by genre
-for genre in genre_list:
-    if st.button(genre):
-        column1, column2 = st.columns(2)
-        column3, column4 = st.columns(2)
+# for genre in genre_list:
+#     if st.sidebar.button(genre):
+#         column1, column2 = st.columns(2)
+#         column3, column4 = st.columns(2)
 
-        try:
-            df = getdata(genre)
-        except:
-            st.write('준비 중입니다...')
+#         try:
+#             df = getdata(genre)
+#         except:
+#             st.write('준비 중입니다...')
 
-        # 1. Keyword
-        with column1:
-            st.write('\n')
-            getkeyword(df)
+#         # 1. Keyword
+#         with column1:
+#             st.write('\n')
+#             getkeyword(df)
 
-        # 2. Similar word
-        with column3:
-            getsimilar(df)
+#         # 2. Similar word
+#         with column3:
+#             getsimilar(df)
 
-        # 3. Keyword Ratio
-        with column2:       # Total
-            st.write('\n')
-            visualize(df, False)
-        with column4:       # Only top 10
-            st.write('\n')
-            visualize(df, True)
+#         # 3. Keyword Ratio
+#         with column2:       # Total
+#             st.write('\n')
+#             visualize(df, False)
+#         with column4:       # Only top 10
+#             st.write('\n')
+#             visualize(df, True)
 
 
 # Sidebar Contents
@@ -245,6 +245,41 @@ st.sidebar.markdown('# 사용 방법')
 text = '##### ``장르``를 선택하면 해당 장르의 ``핵심 키워드``와 함께 ``연관 키워드``, ``키워드별 중요도``를 보여줍니다.'
 st.sidebar.markdown(text)
 st.sidebar.markdown('##### 사이드바를 닫아주시고 장르를 선택해주세요.')
+
+for i in range(len(genre_list)//3):
+    part = genre_list[i*3:i*3+3]
+    a, b, c = st.sidebar.columns(3)
+    with a:
+        a = st.button(part[0])
+    with b:
+        b = st.button(part[1])
+    with c:
+        c = st.button(part[2])
+if a:
+    column1, column2 = st.columns(2)
+    column3, column4 = st.columns(2)
+    try:
+        df = getdata('드라마')
+    except:
+        st.write('준비 중입니다...')
+    genre = '드라마'
+        # 1. Keyword
+    with column1:
+        st.write('\n')
+        getkeyword(df)
+
+    # 2. Similar word
+    with column3:
+        getsimilar(df)
+
+    # 3. Keyword Ratio
+    with column2:       # Total
+        st.write('\n')
+        visualize(df, False)
+    with column4:       # Only top 10
+        st.write('\n')
+        visualize(df, True)
+
 
 for i in range(25):
     st.sidebar.markdown('\n')
